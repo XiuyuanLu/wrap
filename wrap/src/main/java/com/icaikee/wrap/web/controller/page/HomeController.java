0a20461f712f43d60209dc0b26d8e3675e9de132
XiuyuanLu <276908010@qq.com>
2016-06-28 23:13:01 +0800

    v0.0.1

--- wrap/src/main/java/com/icaikee/wrap/web/controller/page/HomeController.java
+++ wrap/src/main/java/com/icaikee/wrap/web/controller/page/HomeController.java
@@ -1,5 +1,12 @@
 package com.icaikee.wrap.web.controller.page;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeController {
 
+	public void test(){
+		
+	}
+	
 }
