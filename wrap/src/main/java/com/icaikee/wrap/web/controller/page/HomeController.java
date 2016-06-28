4fb8d1d57fc7bdfd156e4a1e0df90494026e46c9
XiuyuanLu <276908010@qq.com>
2016-06-29 00:27:42 +0800

    v0.0.1-with probs

--- wrap/src/main/java/com/icaikee/wrap/web/controller/page/HomeController.java
+++ wrap/src/main/java/com/icaikee/wrap/web/controller/page/HomeController.java
@@ -1,12 +1,20 @@
 package com.icaikee.wrap.web.controller.page;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.icaikee.wrap.web.controller.WebContants;
 
 @Controller
+@RequestMapping(WebContants.PAGE)
 public class HomeController {
 
-	public void test(){
-		
+	private final static String HOME_PAGE = "home/home";
+
+	@RequestMapping("/home")
+	public ModelAndView home() {
+		return new ModelAndView(HOME_PAGE);
 	}
-	
+
 }
